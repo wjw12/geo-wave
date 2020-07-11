1002dfb91e209906db41d4cfd59cea3285261a1b
Mapo <ollie@gatech.edu>
2020-07-11 16:45:28 -0700

    show ip textbox

--- src/index.js
+++ src/index.js
@@ -34,29 +34,47 @@ axios.post(baseApi + 'getIPList').then(
         ipList = response.data;
 
         if (ipList) {
-            ipList.forEach(async (element) => {
-                axios.post(baseApi + 'getInfoFromIP', {ip: element}).then(
-                    resp => {
-                        const data = resp.data;
-                        const [y, x] = data.location.coordinates;
-                        const times = data.times;
-                        const size = times.length * 200;
-                        const x_rel = Math.floor((x + 180) / 360 * w - size/2);
-                        const y_rel = Math.floor((90 - y) / 180 * h - size/2);
-                        const radial = 1.0 / Math.log2(times.length + 1);
-                        const el = $('<div></div>')
-                        .css('position', 'fixed')
-                        .css('left', x_rel + 'px')
-                        .css('top', y_rel + 'px')
-                        .css('width', size + 'px')
-                        .css('height', size + 'px')
-                        .css('background', 'repeating-radial-gradient(circle, black, grey ' + radial +'%')
-                        .css('mask-image', 'radial-gradient(circle at center, white, black)')
-                        .css('mix-blend-mode', 'difference');
-                        
-                        $('#content').append(el);
-                    }
-                )
+            // const waveElements = [];
+            const textElements = [];
+
+            Promise.all(
+                ipList.map(async (ip) => {
+                    const resp = await axios.post(baseApi + 'getInfoFromIP', {ip: ip});
+                    const data = resp.data;
+                    const [y, x] = data.location.coordinates;
+                    const x_center = Math.floor((x + 180) / 360 * w);
+                    const y_center = Math.floor((90 - y) / 180 * h);
+                    const times = data.times;
+                    const size = times.length * 200;
+                    const x_rel = Math.floor((x + 180) / 360 * w - size/2);
+                    const y_rel = Math.floor((90 - y) / 180 * h - size/2);
+                    const radial = 1.0 / Math.log2(times.length + 1);
+
+                    const textbox = $('<div><p>' + ip + '</p></div>')
+                    .css('position', 'fixed')
+                    .css('left', x_center + 'px')
+                    .css('top', y_center + 'px');
+
+                    $('#content').append(textbox);
+                    textElements.push(textbox);
+
+                    const el = $('<div></div>')
+                    .css('position', 'fixed')
+                    .css('left', x_rel + 'px')
+                    .css('top', y_rel + 'px')
+                    .css('width', size + 'px')
+                    .css('height', size + 'px')
+                    .css('background', 'repeating-radial-gradient(circle, black, grey ' + radial +'%')
+                    .css('mask-image', 'radial-gradient(circle at center, white, black)')
+                    .css('mix-blend-mode', 'difference')
+
+                    $('#content').append(el);
+                })
+            )
+            .then(() => {
+                textElements.forEach((element) => {
+                    element.css('visibility', 'hidden');
+                })
             })
         }
     }
