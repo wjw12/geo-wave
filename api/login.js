62c8072c709db7ce4eb2bddfbe9a8b1978f6db21
Mapo <ollie@gatech.edu>
2020-07-11 15:29:44 -0700

    add more IP APIs

--- api/login.js
+++ api/login.js
@@ -19,58 +19,69 @@ async function connectToDatabase(uri) {
 module.exports = async (req, res) => {
   // handle CORS preflight
   if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
+    res.status(200).end();
+    return;
   }
   const {body} = req;
   if (!body) return;
-  const db = await connectToDatabase(process.env.MONGODB_URI)
-  const collection = await db.collection('datasource')
+  const db = await connectToDatabase(process.env.MONGODB_URI);
+  const collection = await db.collection('datasource');
+  const ipcache = await db.collection('ipcache');
 
   if (body.ip) {
-      // https://www.twilio.com/blog/2017/08/http-requests-in-node-js.html
-      axios.get('http://ip-api.com/json/' + body.ip)
-        .then(async response => {
-            if (response.data && response.data.status == 'success') {
-                const [lat, lon] = [response.data.lat, response.data.lon];
-                const query = {
-                    location: {
-                        type: 'Point',
-                        coordinates: [lat, lon]
-                    }
-                };
-                const result = await collection.findOne(query);
-                console.log(result);
-                if (result) {
-                    const timeList = result.times;
-                    timeList.push(Date.now())
-                    await collection.update(query, {
-                        times: timeList,
-                        location: {
-                            type: 'Point',
-                            coordinates: [lat, lon]
-                        }
-                    })
-                }
-                else {
-                    await collection.insert({
-                        times: [Date.now()],
-                        location: {
-                            type: 'Point',
-                            coordinates: [lat, lon]
-                        }
-                    })
-                }
+    // if ip exist in cache database, retrieve the location info
+    let geodata = await ipcache.findOne({ip: body.ip});
+    if (!geodata) {
+        const resp = await axios.get('http://ip-api.com/json/' + body.ip);
+        if (resp.data && resp.data.status == 'success') {
+            geodata = resp.data;
+            // insert to cache db
+            await ipcache.insert({ip: body.ip, data: geodata});
+        }
+    } 
+    else {
+        geodata = geodata.data;
+    }
 
+    if (!geodata) {
+        res.status(400).send("fail to retrieve location from IP");
+        return;
+    }
+
+    // update visitor information
+    const [lat, lon] = [geodata.lat, geodata.lon];
+    const query = {
+        location: {
+            type: 'Point',
+            coordinates: [lat, lon]
+        }
+    };
+    const result = await collection.findOne(query);
+    if (result) {
+        const timeList = result.times;
+        timeList.push(Date.now())
+        await collection.update(query, {
+            times: timeList,
+            location: {
+                type: 'Point',
+                coordinates: [lat, lon]
             }
-            res.status(200).send(response.data)
         })
-        .catch(error => {
-            res.status(400).send(error)
-        });
+    }
+    else {
+        await collection.insert({
+            times: [Date.now()],
+            location: {
+                type: 'Point',
+                coordinates: [lat, lon]
+            }
+        })
+    }
+
+    res.status(200).send(response.data);
   }
   else {
-      res.status(400).send("")
+    res.status(400).send("wrong request format")
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -33,17 +33,18 @@ axios.post(baseApi + 'getAll').then(
         response.data.forEach(element => {
             const [y, x] = element.location.coordinates;
             const times = element.times;
-            const size = Math.log2(times.length + 1) * 200;
+            const size = times.length * 200;
             const x_rel = Math.floor((x + 180) / 360 * w - size/2);
             const y_rel = Math.floor((90 - y) / 180 * h - size/2);
-            const radial = 1.0 / times.length * 100;
+            const radial = 1.0 / Math.log2(times.length + 1);
             const el = $('<div></div>')
             .css('position', 'fixed')
             .css('left', x_rel + 'px')
             .css('top', y_rel + 'px')
             .css('width', size + 'px')
             .css('height', size + 'px')
-            .css('background', 'repeating-radial-gradient(circle, black, white ' + radial +'%')
+            .css('background', 'repeating-radial-gradient(circle, black, grey ' + radial +'%')
+            .css('mask-image', 'radial-gradient(circle at center, white, black)')
             .css('mix-blend-mode', 'difference');
             
             $('#content').append(el);
@@ -53,4 +54,4 @@ axios.post(baseApi + 'getAll').then(
 )
 
 window.test();
-setTimeout(() => {location.reload();}, 1000 + 1000 * Math.random());
+//setTimeout(() => {location.reload();}, 5000 + 10 * Math.random());
